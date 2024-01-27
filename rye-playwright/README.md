2ab88b4f54ee4a64e2811f9a1436000837e6d075
nua <enuesaa@gmail.com>
2024-01-28 00:17:08 +0900

    feat(rye): try rye python package manager.

---
+++ rye-playwright/README.md
@@ -0,0 +1,3 @@
+# rye-playwright
+## memo
+- `rye add` や script を追加したときは `rye sync` を実行する. じゃないと反映されない?
---
+++ rye-playwright/src/rye_playwright/app.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import sync_playwright
+import time
+
+def main():
+    pw = sync_playwright().start()
+    browser = pw.chromium.launch(
+        headless=False,
+    )
+    page = browser.new_page()
+    page.goto("http://playwright.dev")
+    print(page.title())
+    time.sleep(10)
+    browser.close()
+    pw.stop()
