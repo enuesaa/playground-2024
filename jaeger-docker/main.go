225986f6f32e847b7c6cc78d3a93f297626591e1
nua <enuesaa@gmail.com>
2024-03-28 02:25:17 +0900

    chore: jaeger ui へ otel の trace を送信したいがうまくいかない

--- jaeger-docker/main.go
+++ jaeger-docker/main.go
@@ -1,28 +1,61 @@
 package main
 
 import (
+	"context"
 	"log"
-	"net/http"
 
-	"github.com/labstack/echo-contrib/jaegertracing"
 	"github.com/labstack/echo/v4"
+
+	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
+	"go.opentelemetry.io/otel"
+	// "go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	// "go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/exporters/trace/jaeger"
+    _ "go.opentelemetry.io/otel/semconv/v1.9.0"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 )
 
-func main() {
-	app := echo.New()
-	app.GET("/", func (c echo.Context) error {
-		res, err := http.Get("https://example.com")
-		if err != nil {
-			return err
-		}
-		defer res.Body.Close()
-		return c.String(200, "aaa")
-	})
+var tracer = otel.Tracer("echo-server")
 
-	c := jaegertracing.New(app, nil)
-	defer c.Close()
-	
-	if err := app.Start(":3000"); err != nil {
+func main() {
+	tp, err := initTracer()
+	if err != nil {
 		log.Fatalf("Error: %s", err.Error())
 	}
+	defer tp.Shutdown(context.Background())
+
+	app := echo.New()
+	app.Use(otelecho.Middleware("a"))
+
+	app.GET("/users/:id", func(c echo.Context) error {
+		return c.String(200, "aaa")
+	})
+	if err = app.Start(":3000"); err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+}
+
+func initTracer() (*sdktrace.TracerProvider, error) {
+	// client := otlptracehttp.NewClient(
+	// 	otlptracehttp.WithEndpoint("10.20.30.40:4318"),
+	// 	otlptracehttp.WithInsecure(),
+	// 	otlptracehttp.WithCompression(otlptracehttp.NoCompression),
+	// )
+	// exporter, err := otlptrace.New(context.Background(), client)
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	endpoint := "http://localhost:14268/api/traces"
+	exp, err := jaeger.NewRawExporter(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(endpoint)))
+	if err != nil {
+		return nil, err
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithBatcher(exp),
+	)
+	otel.SetTracerProvider(tp)
+	return tp, nil
 }
