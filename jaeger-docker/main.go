1e0f2224307bceb6bd76eaaf958673f72077dce5
nua <enuesaa@gmail.com>
2024-03-29 01:46:16 +0900

    feat(jaeger): ローカルでトレースできるようにした

--- jaeger-docker/main.go
+++ jaeger-docker/main.go
@@ -5,24 +5,41 @@ import (
 	"log"
 
 	"github.com/labstack/echo/v4"
+	"go.opentelemetry.io/otel/sdk/trace"
 
 	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
 	"go.opentelemetry.io/otel"
-	// "go.opentelemetry.io/otel/exporters/otlp/otlptrace"
-	// "go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"go.opentelemetry.io/otel/exporters/trace/jaeger"
-    _ "go.opentelemetry.io/otel/semconv/v1.9.0"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 )
 
-var tracer = otel.Tracer("echo-server")
-
 func main() {
-	tp, err := initTracer()
+	ctx := context.Background()
+	client := otlptracehttp.NewClient(
+		otlptracehttp.WithEndpoint("localhost:4318"),
+		otlptracehttp.WithInsecure(),
+	)
+	exporter, err := otlptrace.New(ctx, client)
 	if err != nil {
 		log.Fatalf("Error: %s", err.Error())
 	}
-	defer tp.Shutdown(context.Background())
+
+	resources, err := resource.New(
+		ctx,
+		resource.WithHost(),
+		resource.WithAttributes(
+			semconv.ServiceName("aa"),
+		),
+	)
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		trace.WithResource(resources),
+	)
+	otel.SetTracerProvider(tp)
+	defer tp.Shutdown(ctx)
 
 	app := echo.New()
 	app.Use(otelecho.Middleware("a"))
@@ -33,29 +50,4 @@ func main() {
 	if err = app.Start(":3000"); err != nil {
 		log.Fatalf("Error: %s", err.Error())
 	}
-}
-
-func initTracer() (*sdktrace.TracerProvider, error) {
-	// client := otlptracehttp.NewClient(
-	// 	otlptracehttp.WithEndpoint("10.20.30.40:4318"),
-	// 	otlptracehttp.WithInsecure(),
-	// 	otlptracehttp.WithCompression(otlptracehttp.NoCompression),
-	// )
-	// exporter, err := otlptrace.New(context.Background(), client)
-	// if err != nil {
-	// 	return nil, err
-	// }
-
-	endpoint := "http://localhost:14268/api/traces"
-	exp, err := jaeger.NewRawExporter(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(endpoint)))
-	if err != nil {
-		return nil, err
-	}
-
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithSampler(sdktrace.AlwaysSample()),
-		sdktrace.WithBatcher(exp),
-	)
-	otel.SetTracerProvider(tp)
-	return tp, nil
 }
