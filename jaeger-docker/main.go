39cdd6bb364e765ba4be204ce07b38a9bb570594
nua <enuesaa@gmail.com>
2024-03-30 01:52:56 +0900

    feat(jaeger): refactor sending trace code.

--- jaeger-docker/main.go
+++ jaeger-docker/main.go
@@ -5,49 +5,29 @@ import (
 	"log"
 
 	"github.com/labstack/echo/v4"
-	"go.opentelemetry.io/otel/sdk/trace"
-
 	"go.opentelemetry.io/contrib/instrumentation/github.com/labstack/echo/otelecho"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
-	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 )
 
 func main() {
 	ctx := context.Background()
-	client := otlptracehttp.NewClient(
-		otlptracehttp.WithEndpoint("localhost:4318"),
-		otlptracehttp.WithInsecure(),
-	)
-	exporter, err := otlptrace.New(ctx, client)
+	traceProvider, err := setupTracer(ctx)
 	if err != nil {
 		log.Fatalf("Error: %s", err.Error())
 	}
-
-	resources, err := resource.New(
-		ctx,
-		resource.WithHost(),
-		resource.WithAttributes(
-			semconv.ServiceName("aa"),
-		),
-	)
-	tp := sdktrace.NewTracerProvider(
-		sdktrace.WithBatcher(exporter),
-		trace.WithResource(resources),
-	)
-	otel.SetTracerProvider(tp)
-	defer tp.Shutdown(ctx)
+	defer traceProvider.Shutdown(ctx)
+	tracer := traceProvider.Tracer("example.example")
 
 	app := echo.New()
 	app.Use(otelecho.Middleware("a"))
 
 	app.GET("/users/:id", func(c echo.Context) error {
+		_, span := tracer.Start(c.Request().Context(), "userlookup")
+		defer span.End()
+
 		return c.String(200, "aaa")
 	})
-	if err = app.Start(":3000"); err != nil {
+
+	if err := app.Start(":3000"); err != nil {
 		log.Fatalf("Error: %s", err.Error())
 	}
 }
