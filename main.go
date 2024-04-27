d3ff8c70ede32eeb49a0bfbe8ae683ef49af1d3d
nua <enuesaa@gmail.com>
2024-04-27 17:27:11 +0900

    feat(poc): とりあえず "text/template" でrenderできるようにした

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
-	"fmt"
+	"os"
+	"text/template"
+
 	"github.com/spf13/cobra"
 )
+
+type Data struct {
+	A string `json:"a"`
+}
 
 func main() {
 	app := &cobra.Command{
@@ -12,8 +19,27 @@ func main() {
 		Short:   "template engine",
 		Version: "0.0.1",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("a")
-			return nil
+			fbyte, err := os.ReadFile("testdata/data.json")
+			if err != nil {
+				return err
+			}
+
+			var data Data
+			if err := json.Unmarshal(fbyte, &data); err != nil {
+				return err
+			}
+
+			tmplbyte, err := os.ReadFile("testdata/a.txt")
+			if err != nil {
+				return err
+			}
+
+			tmpl, err := template.New("try").Parse(string(tmplbyte))
+			if err != nil {
+				return err
+			}
+		
+			return tmpl.Execute(os.Stdout, data)
 		},
 	}
 
