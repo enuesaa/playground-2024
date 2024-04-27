fd1113fc8df91bf5684025c22157f247b6c9d97c
nua <enuesaa@gmail.com>
2024-04-27 17:04:59 +0900

    feat(cli): use cobra

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"log"
 	"fmt"
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	fmt.Println("a")
+	app := &cobra.Command{
+		Use:     "txtsout",
+		Short:   "template engine",
+		Version: "0.0.1",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println("a")
+			return nil
+		},
+	}
+
+	// disable default
+	app.SetHelpCommand(&cobra.Command{Hidden: true})
+	app.CompletionOptions.DisableDefaultCmd = true
+	app.SilenceErrors = true
+	app.SilenceUsage = true
+	app.PersistentFlags().SortFlags = false
+	app.PersistentFlags().BoolP("help", "", false, "Show help information")
+	app.PersistentFlags().BoolP("version", "", false, "Show version")
+
+	if err := app.Execute(); err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
 }
