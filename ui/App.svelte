c7ce15d2d88f83a4ca6b38d0ece7402ea7839247
nua <enuesaa@gmail.com>
2024-04-30 03:14:41 +0900

    feat(ui): use svelte-routing

--- ui/App.svelte
+++ ui/App.svelte
@@ -1,3 +1,12 @@
 <script lang="ts">
+	import { Router, Route } from 'svelte-routing'
+	import Home from './routes/Home.svelte'
+	import About from './routes/About.svelte'
+
+	export let url = ''
 </script>
 
+<Router {url}>
+	<Route path="/about" component={About} />
+	<Route path="/"><Home /></Route>
+</Router>
