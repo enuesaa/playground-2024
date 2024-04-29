f7afcdca9c3250ef3a6851bfd146d4a14c145d59
nua <enuesaa@gmail.com>
2024-04-30 02:05:15 +0900

    feat(cli): use kong as flag parser

--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/alecthomas/kong"
+)
+
+var cli struct {
+	Port int `help:"port to serve." default:"3000"`
+}
+
 func main() {
+	kong.Parse(&cli)
+	fmt.Println(cli.Port)
 }
