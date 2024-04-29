b85870f1209c4bca5a156d3bbf3fb44a619e53be
nua <enuesaa@gmail.com>
2024-04-30 02:42:20 +0900

    feat(cli): change flag name

--- main.go
+++ main.go
@@ -1,24 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/alecthomas/kong"
 )
 
 var cli struct {
-	Serve bool `help:"serve" default:"false"`
+	Browser bool `short:"b" help:"open in web browser" default:"false"`
 	Port int `help:"port to serve." default:"3000"`
 }
 
 func main() {
 	kong.Parse(&cli)
-	fmt.Println(cli.Port)
 
-	if cli.Serve {
-		if err := Serve(); err != nil {
-			log.Fatalf("Error: %s", &err)
+	if cli.Browser {
+		if err := Serve(cli.Port); err != nil {
+			log.Fatalf("Error: %s", err.Error())
 		}
 	}
 }
--- serve.go
+++ serve.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func Serve() error {
+func Serve(port int) error {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
 
-	return http.ListenAndServe(":3000", r)
+	addr := fmt.Sprintf(":%d", port)
+	return http.ListenAndServe(addr, r)
 }
