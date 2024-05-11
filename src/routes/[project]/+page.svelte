ae6457ff7bc0b6292e0aa1bf0ccbcabc8008484b
nua <enuesaa@gmail.com>
2024-05-11 20:07:11 +0900

    chore: 完成物のイメージを書き出した

---
+++ src/routes/[project]/+page.svelte
@@ -0,0 +1,19 @@
+project page
+
+// readme
+// flow
+// - title: 'create app'
+//   description: ''
+//   files:
+//     - file
+//     - file
+// - title: 'run command'
+//   description: ''
+//   console: |
+//     $ echo 'a'
+//     a
+// - title: 'add file'
+//   description: ''
+//   files:
+//     - file
+//     - file
