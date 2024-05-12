66bfc1f8bc5da4813d644d2075c376468af558b8
nua <enuesaa@gmail.com>
2024-05-13 02:07:06 +0900

    feat: styling

--- src/routes/[project]/+page.svelte
+++ src/routes/[project]/+page.svelte
@@ -4,43 +4,30 @@
 
 <div class="container mx-auto m-5 p-3">
 	<h2 class="text-3xl py-5 font-zenkaku font-semibold">
-		タイトル
+		cobra でサブコマンドを作成する
 	</h2>
 	<div class="flex">
 		<div class="flex-auto">
 			<ul class="my-6 py-5 px-5 text-lg list-disc" style="border: solid 1px #999;">
-				<li>状況説明</li>
-				<li>背景</li>
-				<li>モチベーション・解決したい課題とか</li>
+				<li>cobra はGoでCLIツールを作る時便利なライブラリ</li>
+				<li>サブコマンドを作成するコード</li>
 			</ul>
-			<h3>[課題感] 〇〇でxxできない</h3>
+			<h3>ファイル一覧</h3>
 			<div class="my-6 py-5 text-lg relative" style="border: solid 1px #999;">
 				<pre>
-					$ pnpm create next-app
+					// vscode みたいに左ペインにファイル一覧が表示され、開いているファイルが横に並んでいる
 
-					$ ls
+					- main.go
+					- cmd.go
 				</pre>
-				<button class="absolute top-0 right-0">ファイル一覧</button>
 			</div>
-			<h3>[解決策a] 〇〇する</h3>
+			<h3>コンソール</h3>
 			<div class="my-6 py-5 text-lg relative" style="border: solid 1px #999;">
 				<pre>
-					$ pnpm create next-app
-
-					$ ls
+					$ aaa bb
+					hello
 				</pre>
-				<button class="absolute top-0 right-0">ファイル一覧</button>
 			</div>
-			<h3>[解決策b] 〇〇する</h3>
-			<div class="my-6 py-5 text-lg relative" style="border: solid 1px #999;">
-				<pre>
-					$ pnpm create next-app
-
-					$ ls
-				</pre>
-				<button class="absolute top-0 right-0">ファイル一覧</button>
-			</div>
-			
 		</div>
 		<div class="w-52"></div>
 	</div>
@@ -49,4 +36,5 @@
 <pre>
 	// ステップを作ったらブログと変わらない気がする
 	// 主題と関係ないファイルは一覧にだけ表示して、中身は省略したい
+	// TODO: 実際に書く内容. example で見つかるような一般的な内容は書きたくない
 </pre>
