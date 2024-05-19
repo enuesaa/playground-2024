f247b907c4baf2bef40a6283beaae32b8111da57
nua <enuesaa@gmail.com>
2024-05-19 19:54:09 +0900

    feat(ui): fix styling of flex layout

--- src/routes/[name]/CodeViewer.svelte
+++ src/routes/[name]/CodeViewer.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <section>
-	<ul class="w-56" {...$tree}>
+	<ul class="w-56 flex-none" {...$tree}>
 		<CodeTree {treeData} />
 	</ul>
 	<div class="flex-auto">
@@ -30,6 +30,6 @@
 	section {
 		border: solid 1px rgba(0, 0, 0, 0.2);
 		box-shadow: 0 1px 1px rgba(255, 255, 255, 0.2);
-		@apply flex gap-4 my-5 bg-grayer;
-	}
+		@apply flex gap-4 my-5 bg-grayer overflow-y-scroll h-96;
+	}	
 </style>
