7ed8962a8a9f5e668f5ae009fec104161c331bcf
nua <enuesaa@gmail.com>
2024-05-26 15:58:48 +0900

    feat(ui): styling of tabs.

--- src/routes/[name]/+page.svelte
+++ src/routes/[name]/+page.svelte
@@ -6,6 +6,7 @@
 	import Description from './Description.svelte'
 	import type { PageData } from './$types'
 	import Output from './Output.svelte'
+	import VariantNavButton from './VariantNavButton.svelte'
 
 	export let data: PageData
 	let showingVariantName = data.variants[0].name
@@ -14,21 +15,20 @@
 <PageTitle title={data.title} />
 <Description content={data.description} />
 
-<nav>
+<nav class="m-0">
 	{#each data.variants as variant}
-		<button class="m-3"
-			style="border: solid 1px #111;"
-			on:click|preventDefault={() => showingVariantName = variant.name}
-		>{variant.name}</button>
+		<VariantNavButton bind:showing={showingVariantName} name={variant.name} />
 	{/each}
 </nav>
 
-{#each data.variants as variant}
-	{#if variant.name === showingVariantName}
-		<Provider>
-			<VariantTitle title={variant.title} />
-			<CodeViewer treeData={variant.files} />
-			<Output output={variant.output} />
-		</Provider>
-	{/if}
-{/each}
+<section class="border-2 px-3 py-2 rounded-lg">
+	{#each data.variants as variant}
+		{#if variant.name === showingVariantName}
+			<Provider>
+				<VariantTitle title={variant.title} />
+				<CodeViewer treeData={variant.files} />
+				<Output output={variant.output} />
+			</Provider>
+		{/if}
+	{/each}
+</section>
---
+++ src/routes/[name]/VariantNavButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	export let name: string
+	export let showing: string
+
+	function handleClick() {
+		showing = name
+	}
+</script>
+
+<button
+	on:click|preventDefault={handleClick}
+	class={showing === name ? 'bg-grayer' : 'bg-gray'}
+>
+	{name}
+</button>
+
+<style lang="postcss">
+	button {
+		border: solid 2px;
+		border-bottom: 0px;
+		@apply mt-3 mx-1 p-3 border-black;
+		@apply rounded-t-lg;
+	}
+</style>
