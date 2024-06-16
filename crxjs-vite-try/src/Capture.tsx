2b9a05c91a6fe6d1fc763040865925b07afa4f43
nua <enuesaa@gmail.com>
2024-06-16 23:48:32 +0900

    feat(crxjs): call getLayoutMetrics to calc page size

--- crxjs-vite-try/src/Capture.tsx
+++ crxjs-vite-try/src/Capture.tsx
@@ -18,15 +18,39 @@ export const Capture = () => {
       return
     }
     const tab = tabs[0]
-    console.log(tab)
+    const tabId = tab.id
 
-    const a = await chrome.tabs.captureVisibleTab()
-    console.log(a)
+    await chrome.debugger.attach({ tabId }, '1.3')
+
+    // chrome devtool protocol
+    // see https://zenn.dev/kakkoyakakko/articles/54fe29dc3751b9
+    // see https://github.com/microsoft/playwright/blob/2ae2fb421c6b6ec115721a8b0a5215442e5411c3/packages/playwright-core/src/server/chromium/crPage.ts#L279
+    const layoutMetrics = await chrome.debugger.sendCommand({ tabId }, 'Page.getLayoutMetrics', {})
+    console.log(layoutMetrics)
+    const res = await chrome.debugger.sendCommand({ tabId }, 'Page.captureScreenshot', {
+      format: 'png',
+      clip: {
+        "x": 0, 
+        "y": 0,
+        // @ts-ignore
+        "width": layoutMetrics.cssLayoutViewport.pageX,
+        // @ts-ignore
+        "height": layoutMetrics.cssLayoutViewport.pageY,
+        "scale": 1
+      },
+      // captureBeyondViewport: true,
+    })
+
+    // @ts-ignore
+    const imageUrl: string = `data:image/png;base64,${res.data}`
+    console.log(imageUrl)
 
     await chrome.downloads.download({
-      url: a,
+      url: imageUrl,
       filename: 'capture.png',
     })
+
+    await chrome.debugger.detach({ tabId })
   };
 
   return <button onClick={handleClick}>capture</button>
