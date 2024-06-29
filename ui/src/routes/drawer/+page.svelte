ed9e2ff56498dc20019ecabf7f5ca82ea004e726
nua <enuesaa@gmail.com>
2024-06-29 16:21:09 +0900

    feat(drawer): add circle on click event

---
+++ ui/src/routes/drawer/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	type Circle = {
+		cx: number;
+		cy: number;
+		r: number;
+	}
+    let circles: Circle[] = [];
+
+    function handleClick(e: MouseEvent & {currentTarget: EventTarget & SVGSVGElement}) {
+        const { left, top } = e.currentTarget.getBoundingClientRect()
+		const circle = {
+			cx: e.clientX - left,
+			cy: e.clientY - top,
+			r: 20,
+		}
+        circles = [...circles, circle];
+    }
+</script>
+
+<svg on:click|preventDefault={handleClick}>
+    {#each circles as { cx, cy, r }}
+        <circle cx={cx} cy={cy} r={r} fill="#000000" />
+    {/each}
+</svg>
+
+<style lang="postcss">
+    svg {
+        width: 100%;
+        height: 100vh;
+    }
+</style>
