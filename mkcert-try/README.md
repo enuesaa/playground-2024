ac6adb48b4ae61e517b494c71f7fe85edbba8d0b
nua <enuesaa@gmail.com>
2024-07-10 23:59:35 +0900

    feat: try mkcert with next.js

---
+++ mkcert-try/README.md
@@ -0,0 +1,17 @@
+# mkcert
+
+## memo
+- ローカル開発用の証明書を作れるCLIツール
+- Go
+- だいぶ前に使ったことあるかも
+
+## Links
+- https://github.com/FiloSottile/mkcert
+
+## Commands
+```bash
+brew install mkcert
+brew install nss
+mkcert -install
+mkcert 0.0.0.0
+```
---
+++ mkcert-try/samplesite/src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import '@radix-ui/themes/styles.css'
+import { Theme } from '@radix-ui/themes'
+import '@/styles/app.css'
+
+export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient()
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Theme appearance='dark'>
+        <Component {...pageProps} />
+      </Theme>
+    </QueryClientProvider>
+  )
+}
