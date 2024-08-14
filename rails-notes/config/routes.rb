ddf437c52af7ae3c1823b717ecd082df560a1a4a
nua <enuesaa@gmail.com>
2024-08-15 00:40:30 +0900

    feat(rails-notes): 不要な routing を削除. ヘルスチェック用のコントローラーがデフォルトで用意されているっぽい

--- rails-notes/config/routes.rb
+++ rails-notes/config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  root "notes#index"
+  # see https://github.com/rails/rails/blob/main/railties/lib/rails/health_controller.rb#L38
+  root "rails/health#show"
 
   get "notes", to: "notes#index" 
   post "notes", to: "notes#create"
-  get "notes/:id", to: "notes#show", as: :note
+  get "notes/:id", to: "notes#show" #, as: :note  # 名前付きルート. note_path や note_url という関数を生成するらしい
   put "notes/:id", to: "notes#update"
   delete "notes/:id", to: "notes#destroy"
-
-  # or
   # resources :notes, except: [:new, :edit]
-
-  get "up" => "rails/health#show", as: :rails_health_check
 end
