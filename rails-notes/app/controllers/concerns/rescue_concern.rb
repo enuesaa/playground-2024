c69474faf81f21d5da02bd6957f3633c4f0afe3d
nua <enuesaa@gmail.com>
2024-08-15 00:51:19 +0900

    feat(rails-notes): 例外キャッチロジックを concern に切り出した

---
+++ rails-notes/app/controllers/concerns/rescue_concern.rb
@@ -0,0 +1,19 @@
+# 他言語でいう trait 的なもの
+module RescueConcern
+  extend ActiveSupport::Concern
+
+  # たぶん、これが module のなかに定義しているから、読み込み元で実行するよう included を実行している
+  included do
+    rescue_from ActionController::ParameterMissing, with: :handle_parameter_missing
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+  end
+
+  private
+    def handle_parameter_missing(e)
+      render status: 400, json: { message: e.original_message }
+    end
+
+    def handle_record_not_found
+      render status: 404, json: { message: "not found" }
+    end
+end
