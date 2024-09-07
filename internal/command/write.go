89928fccc2fa2724d9359c5a8e43dd2ac2a864af
nua <enuesaa@gmail.com>
2024-09-07 20:17:17 +0900

    feat(write): save data as markdown

--- internal/command/write.go
+++ internal/command/write.go
@@ -1,6 +1,8 @@
 package command
 
 import (
+	"fmt"
+
 	"github.com/enuesaa/codetrailer/internal/repository"
 	"github.com/enuesaa/codetrailer/internal/usecase"
 	"github.com/urfave/cli/v2"
@@ -10,11 +12,17 @@ func NewWriteCommand(repos repository.Repos) *cli.Command {
 	cmd := &cli.Command{
 		Name:  "write",
 		Args: true,
+		ArgsUsage: "<filename>",
 		Before: func(ctx *cli.Context) error {
 			return usecase.CreateRegistry(repos)
 		},
 		Action: func(c *cli.Context) error {
-			go usecase.Write(repos)
+			filename := c.Args().Get(0)
+			if filename == "" {
+				return fmt.Errorf("filename is required")
+			}
+
+			go usecase.Write(repos, filename)
 
 			return usecase.LaunchMenu()
 		},
--- internal/usecase/write.go
+++ internal/usecase/write.go
@@ -2,11 +2,12 @@ package usecase
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/enuesaa/codetrailer/internal/repository"
 )
 
-func Write(repos repository.Repos) error {
+func Write(repos repository.Repos, filename string) error {
 	texts := []string{}
 
 	for {
@@ -24,6 +25,14 @@ func Write(repos repository.Repos) error {
 		} else {
 			texts = append(texts, text)
 		}
+	}
+
+	body := strings.Join(texts, "\n")
+	bodyreader := strings.NewReader(body)
+
+	path := fmt.Sprintf(".codetrailer/%s.md", filename)
+	if err := repos.Fs.Create(path, bodyreader); err != nil {
+		return err
 	}
 
 	return nil
