0a1d05c30619f658e24dffaa4615cec19bf7be78
nua <enuesaa@gmail.com>
2024-09-21 23:33:23 +0900

    feat(write): start echo server on write command

--- internal/command/write.go
+++ internal/command/write.go
@@ -1,33 +1,19 @@
 package command
 
 import (
-	"fmt"
 
 	"github.com/enuesaa/codetrailer/internal/repository"
-	"github.com/enuesaa/codetrailer/internal/usecase"
+	"github.com/enuesaa/codetrailer/internal/router"
 	"github.com/urfave/cli/v2"
 )
 
 func NewWriteCommand(repos repository.Repos) *cli.Command {
 	cmd := &cli.Command{
 		Name:  "write",
-		Args: true,
-		ArgsUsage: "<path>",
-		Before: func(ctx *cli.Context) error {
-			if ctx.Args().Len() == 0 {
-				return fmt.Errorf("path is required")
-			}
-			return nil
-		},
 		Action: func(c *cli.Context) error {
-			path := c.Args().First()
+			app := router.New(repos)
 
-			if err := usecase.Setup(repos, path); err != nil {
-				return err
-			}
-			go usecase.Write(repos, path)
-
-			return usecase.LaunchMenu(repos, path)
+			return app.Start(":3000")
 		},
 	}
 
