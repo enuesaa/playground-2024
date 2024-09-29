7f948114ba441f20bdc4374ea2d90f4a4eb30f1a
nua <enuesaa@gmail.com>
2024-09-29 21:00:55 +0900

    feat: create pdf file with playwright

---
+++ playwright-pdf/go.mod
@@ -0,0 +1,25 @@
+module github.com/enuesaa/plyground-2024/playwright-pdf
+
+go 1.23.0
+
+require (
+	github.com/pdfcpu/pdfcpu v0.8.1
+	github.com/playwright-community/playwright-go v0.4702.0
+)
+
+require (
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/tiff v1.0.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/exp v0.0.0-20240506185415-9bf2ced13842 // indirect
+	golang.org/x/image v0.19.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ playwright-pdf/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/playwright-community/playwright-go"
+)
+
+func main() {
+	if err := playwright.Install(); err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+
+	pw, err := playwright.Run()
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+	browser, err := pw.Chromium.Launch()
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+	page, err := browser.NewPage(playwright.BrowserNewPageOptions{
+		Screen: &playwright.Size{
+			Width: 1000,
+			Height: 500,
+		},
+	})
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+	_, err = page.Goto("https://example.com")
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+
+	fbytes, err := page.PDF(playwright.PagePdfOptions{
+		Width: playwright.String("1000"),
+		Height: playwright.String("500"),
+	})
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+	f, err := os.Create("a.pdf")
+	defer f.Close()
+	f.Write(fbytes)
+
+	if err := api.MergeCreateFile([]string{"a.pdf", "a.pdf"}, "o.pdf", false, nil); err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+}
