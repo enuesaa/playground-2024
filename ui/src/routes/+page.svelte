e176c1c18b115ad8ad79f3866fd5f021bf54bd59
nua <enuesaa@gmail.com>
2024-09-30 16:47:57 +0900

    feat(ui): styling of slides

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	let content = ""
+	import Doc from './Doc.svelte'
+
+	let content = ''
 </script>
 
 <section class="w-full flex h-full">
 	<div class="w-[1000px] h-[800px] px-5 py-8 border-2">
-		{content}
+		<Doc {content} />
 	</div>
 	<div class="flex-auto">
 		<textarea bind:value={content} />
@@ -14,6 +16,6 @@
 <style lang="postcss">
 	textarea {
 		@apply block h-full bg-[rgba(0,0,0,0)];
-		@apply font-normal w-full rounded px-3 py-2 block h-[80vh] text-black text-lg outline-none;
+		@apply font-normal w-full p-3 text-black text-lg outline-none;
 	}
 </style>
---
+++ ui/src/routes/Doc.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Markdown from 'svelte-exmarkdown'
+
+	export let content: string
+</script>
+
+<div class="appdoc">
+	<Markdown md={content} />
+</div>
+
+<!-- svelte-ignore css-unused-selector -->
+<style lang="postcss">
+	.appdoc {
+		@apply py-1 px-2;
+	}
+	:global(.appdoc h1) {
+		@apply text-6xl mt-40 mb-5 font-bold;
+	}
+	:global(.appdoc h2) {
+		@apply text-5xl my-1 font-bold;
+	}
+	:global(.appdoc p) {
+		@apply my-1;
+	}
+	:global(.appdoc ul) {
+		@apply my-1 pl-3;
+	}
+	:global(.appdoc li) {
+		list-style-type: "- ";
+	}
+</style>
