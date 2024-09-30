0846300ae018d216c74a947f63ebf08980fb9e8b
nua <enuesaa@gmail.com>
2024-10-01 01:46:21 +0900

    feat(slide): load local asset

--- ui/serve.go
+++ ui/serve.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 
 	"github.com/labstack/echo/v4"
+	"github.com/enuesaa/codetrailer/internal/router/ctx"
 )
 
 //go:generate pnpm install
@@ -17,17 +18,26 @@ import (
 var dist embed.FS
 
 func Serve(c echo.Context) error {
-	path := c.Request().URL.Path // like `/`
-	path = fmt.Sprintf("dist%s", path)
+	cc := ctx.Use(c)
 
-	fileExt := filepath.Ext(path)
-	if fileExt == "" || strings.HasSuffix(path, "/") {
-		path = "dist/index.html"
+	path := cc.Request().URL.Path // like `/index.html`
+	path = strings.TrimPrefix(path, "/") // like `index.html`
+
+	distpath := fmt.Sprintf("dist/%s", path)
+	fileExt := filepath.Ext(distpath)
+	if fileExt == "" {
+		distpath = "dist/index.html"
 	}
 
-	f, err := dist.ReadFile(path)
+	f, err := dist.ReadFile(distpath)
 	if err != nil {
-		return err
+		// read files in current path
+		assetdir := filepath.Dir(cc.Repos.Config.DocPath)
+		assetpath := filepath.Join(assetdir, path)
+		f, err = cc.Repos.Fs.Read(assetpath)
+		if err != nil {
+			return err
+		}
 	}
 	mimeType := mime.TypeByExtension(fileExt)
 
