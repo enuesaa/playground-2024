5149b7876ba50a692773e4c7a3231bc06ec8becb
nua <enuesaa@gmail.com>
2024-12-14 15:24:09 +0900

    feat: try to use html-to-image

--- src/routes/Drawer.svelte
+++ src/routes/Drawer.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
+	import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image'
+
+	let canvas: undefined|HTMLDivElement = undefined;
 	let style = $state('')
 	let border = $state('none')
+	let imgsrc = $state('')
 
 	$effect(() => {
 		console.log(border)
 		style = `border: ${border};`
 	})
+
+	async function handleClick() {
+		if (canvas === undefined) {
+			return
+		}
+		const dataurl = await toPng(canvas)
+		imgsrc = dataurl
+	}
 </script>
 
 border
 <input type="text" bind:value={border} />
 
-<div {style}>
+<div {style} bind:this={canvas}>
 	hello
 </div>
+
+<button onclick={handleClick}>toPng</button>
+
+<img src={imgsrc} alt='' />
