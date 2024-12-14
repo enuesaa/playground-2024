a2741d72409d3a1c5207d7e7eecf92de144e6c9d
nua <enuesaa@gmail.com>
2024-12-14 14:35:31 +0900

    feat: minimum poc

---
+++ src/routes/Drawer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let style = ''
+	let border = 'none'
+
+	$: {
+		console.log(border)
+		style = `border: ${border};`
+	}
+	
+</script>
+
+border
+<input type="text" bind:value={border} />
+
+<div {style}>
+	hello
+</div>
